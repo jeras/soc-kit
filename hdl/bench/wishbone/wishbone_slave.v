5881b8aa11200b1ae1d1b8d39f86b21a2c438f68
Iztok Jeras <izi@agrajag.(none)>
2009-01-25 23:51:12 +0100

    corrected slave response timing hadler

--- hdl/bench/wishbone/wishbone_slave.v
+++ hdl/bench/wishbone/wishbone_slave.v
@@ -27,8 +27,8 @@ module wishbone_slave #(
   // bus parameters
   parameter AW = `WB_AW,         // address bus width
   parameter DW = `WB_DW,         // data bus width
-  parameter SW = `WB_DW/8,       // byte select bus width
-  parameter DE = `WB_DE,         // data endianness ('BIG' or 'LITTLE')
+  parameter SW =     DW/8,       // byte select bus width
+  parameter DE = `WB_DE,         // data endianness ("be"-big or "le"-little)
   parameter IV = 1'bx,           // idle value (value of signals when bus is idle)
   // memory parameters
 //  parameter integer MS = 1024,  // memory size (in Bytes)
@@ -36,15 +36,10 @@ module wishbone_slave #(
   parameter MS = 1024,  // memory size (in Bytes)
   parameter AM = 1023,  // address mask
   // write and read latencies for sequential and nonsequential accesses
-  parameter CW  = 8,    // time counter width
-//  parameter [CW-1:0] lw  = 0,    // write latency for first transfer
-//  parameter [CW-1:0] lr  = 0,    // read  latency for first transfer
-//  parameter [CW-1:0] lwb = 0,    // write latency for burst transfers
-//  parameter [CW-1:0] lrb = 0     // read  latency for burst transfers
-  parameter  lw  = 0,    // write latency for first transfer
-  parameter  lr  = 0,    // read  latency for first transfer
-  parameter  lwb = 0,    // write latency for burst transfers
-  parameter  lrb = 0     // read  latency for burst transfers
+  parameter lw  = 0,    // write latency for first transfer
+  parameter lr  = 0,    // read  latency for first transfer
+  parameter lwb = 0,    // write latency for burst transfers
+  parameter lrb = 0     // read  latency for burst transfers
 )(
   // Wishbone system signals
   input  wire          clk,      // system clock
@@ -75,20 +70,20 @@ module wishbone_slave #(
 //////////////////////////////////////////////////////////////////////////////
 
 // slave control signal
-wor           error_req;
+wor    error_req;
 
 assign error_req = 1'b0;
 assign error_req = error;          // default error value
 
-// cycle and burst length couners
-integer cnt_t;    // time counter
-
 // slave memory
-reg     [7:0] mem [0:MS-1];
+reg  [7:0] mem [0:MS-1];
 
 // transfer response
-wire          trn;      // transfer completion indicator
-wire          rdy;      // ready for new cycle
+wire       trn;      // transfer completion indicator
+wire       rdy;      // ready for new cycle
+wire       stop;     // time for cycle acknowledge
+reg        first;    // indicator of first transfer in burst cycle
+integer    cnt;      // cycle length counter
 
 // byte select
 genvar        i;
@@ -100,27 +95,34 @@ genvar        i;
 assign trn = cyc & stb & (ack | err | rty);
 assign rdy = ~cyc | trn & ((cti == 3'b000) | (cti == 3'b111));
 
-
-// cycle and burst length couners
-// generating the acknowledge signal with the proper timing
+// signaling the first cycle in a burst
 always @(posedge rst, posedge clk)
-if (rst)        cnt_t <= 0;
+if (rst)         first <= 1;
 else begin
-  if (cyc & stb) begin
-    if (trn) begin
-      if (rdy)  cnt_t <= (we) ? lw  : lr;
-      else      cnt_t <= (we) ? lwb : lrb;
-    end else
-                cnt_t <= cnt_t - 1;
+  if (rdy)       first <= 1;
+  else if (trn)  first <= 0;
+end
+
+// cycle length counter
+always @(posedge rst, posedge clk)
+if (rst)      cnt <= 0;
+else begin
+  if (rdy)    cnt <= 0;
+  else if (cyc & stb) begin
+    if (trn)  cnt <= 0;
+    else      cnt <= cnt + 1;
   end
 end
 
+assign stop = cnt == ((first) ? ((we) ? lw  : lr )
+                              : ((we) ? lwb : lrb));
+
 // acknowledge response
-assign ack = (cyc) ? (cnt_t == 0) : IV;
+assign ack = (cyc) ? stop : IV;
 // error response
-assign err = (cyc) ? 1'b0         : IV;  // TODO
+assign err = (cyc) ? 1'b0 : IV;  // TODO
 // retry response
-assign rty = (cyc) ? 1'b0         : IV;  // TODO
+assign rty = (cyc) ? 1'b0 : IV;  // TODO
 
 //////////////////////////////////////////////////////////////////////////////
 // memory and data bus implementation                                       //
