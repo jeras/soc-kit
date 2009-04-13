fc9f94c46e1820f3f641860583616dccf90c949b
Iztok Jeras <izi@agrajag.(none)>
2009-04-13 19:46:01 +0200

    added baudrate timing, and corrected few bugs

--- hdl/bench/uart/uart_model.v
+++ hdl/bench/uart/uart_model.v
@@ -26,8 +26,9 @@
 module uart_model #(
   parameter DW     = 8,          // data width (sizes from 5 to 8 bits are supported)
   parameter SW     = 1,          // stop width (one or more stop bits are supported)
+  parameter IDLE   = 1'b1,       // bus idle state
   parameter PARITY = "NONE",     // parity ("NONE" , "EVEN", "ODD")
-  parameter BAUD   = 112.200,    // baud rate
+  parameter BAUD   = 112_200,    // baud rate
   // data streams
   parameter FILE_I = "",         // program filename
   parameter FILE_O = "",         // program filename
@@ -46,11 +47,17 @@ module uart_model #(
 );
 
 ///////////////////////////////////////////////////////////////////////////////
+// computing the delay based on the baudrate                                 //
+///////////////////////////////////////////////////////////////////////////////
+
+localparam d = 1_000_000_000/BAUD;
+
+///////////////////////////////////////////////////////////////////////////////
 // internal signals                                                          //
 ///////////////////////////////////////////////////////////////////////////////
 
 // running status
-reg run;
+reg run_tx, run_rx;
 
 //            transmitter, receiver   ;
 integer       fp_tx , fp_rx ;
@@ -59,6 +66,9 @@ integer       cnt_tx, cnt_rx;
 integer       bit_tx, bit_rx;
 reg [DW-1:0]  c_tx  , c_rx  ;  // transferred character
 
+// transfer start condition and data sampling events for UART receiver
+event RxD_edge, sample;
+
 ///////////////////////////////////////////////////////////////////////////////
 // file handler                                                              //
 ///////////////////////////////////////////////////////////////////////////////
@@ -66,7 +76,9 @@ reg [DW-1:0]  c_tx  , c_rx  ;  // transferred character
 // automatic initialization if enabled
 initial begin
   $display ("DEBUG: Starting master");
-  TxD = 1'b1;
+  TxD = IDLE;
+  run_tx = 0;
+  run_rx = 0;
   if (AUTO)  start (FILE_I, FILE_O);
 end
 
@@ -87,12 +99,14 @@ task start (
     fp_rx = $fopen (filename_rx, "w");
     $display ("DEBUG: Opening read file for output stream \"%0s\".", filename_rx);
   end
-  run = 1;
+  run_rx <= 1;
+  run_tx <= 1;
 end endtask
 
 // stop UART model
 task stop; begin
-  run = 0;
+  run_tx = 0;
+  run_rx = 0;
   $fclose (fp_tx);
   $fclose (fp_rx);
 end endtask
@@ -102,57 +116,61 @@ end endtask
 ///////////////////////////////////////////////////////////////////////////////
 
 // transmitter
-initial begin
-  #50;
-  while (run) begin
+always @ (posedge run_tx) begin
+  while (run_tx) begin
+    while ($feof(fp_tx)) begin end
     c_tx = $fgetc(fp_tx);
     // start bit
-    TxD = 1'b0;
-    #10;
+    TxD = ~IDLE; #d;
     // transmit bits LSB first
     for (bit_tx=0; bit_tx<DW; bit_tx=bit_tx+1) begin
       //{c_tx [DW-2:0], TxD} = c_tx;
-      TxD = c_tx [bit_tx];
-      #10;
+      TxD = c_tx [bit_tx]; #d;
     end
     // send parity
     case (PARITY)
-      "ODD"  : begin  TxD = ~^c_tx; #10;  end
-      "EVEN" : begin  TxD =  ^c_tx; #10;  end
-      "NONE" : begin                        end
+      "ODD"  : begin  TxD = ~^c_tx; #d;  end
+      "EVEN" : begin  TxD =  ^c_tx; #d;  end
+      "NONE" : begin                     end
     endcase
     // increment counter
     cnt_tx = cnt_tx + 1;
     // stop bits
     for (bit_tx=DW; bit_tx<DW+SW; bit_tx=bit_tx+1) begin
-      TxD = 1'b1;
-      #10;
+      TxD = IDLE; #d;
     end
   end
 end
 
 // receiver
-initial begin
-  while (run) begin
-    @ (negedge RxD) begin
-      // skip start bit
-      #5;
+generate
+if (IDLE)  always @ (negedge RxD) -> RxD_edge;
+else       always @ (posedge RxD) -> RxD_edge;
+endgenerate
+
+always @ (posedge run_rx) begin
+  while (run_rx) begin
+    @ (RxD_edge) begin
+      #(d/2);
+      // check the start bit
+      if (RxD != ~IDLE)  $display ("DEBUG: start bit error."); #d;
       // sample in the middle of each bit
       for (bit_rx=0; bit_rx<DW; bit_rx=bit_rx+1) begin
-        #10;
         //c_rx = {c_rx [DW-2:0], RxD};
-        c_rx [bit_rx] = RxD;
+        -> sample;
+        c_rx [bit_rx] = RxD; #d;
       end
       // check parity
       case (PARITY)
-        "ODD"  : begin  #10; if (RxD != ~^c_rx)  $display ("DEBUG: parity error.");  end
-        "EVEN" : begin  #10; if (RxD !=  ^c_rx)  $display ("DEBUG: parity error.");  end
-        "NONE" : begin                                                                 end
+        "ODD"  : begin  if (RxD != ~^c_rx)  $display ("DEBUG: parity error."); #d;  end
+        "EVEN" : begin  if (RxD !=  ^c_rx)  $display ("DEBUG: parity error."); #d;  end
+        "NONE" : begin                                                              end
       endcase
-      // increment counter
+      // increment counter and write received character into file
       cnt_rx = cnt_rx + 1;
-      // skip the stop bit
-      $fwrite (fp_rx, "%s", c_rx);
+      fs_tx = $ungetc (c_rx, fp_rx);
+      // check the stop bit
+      if (RxD != IDLE)  $display ("DEBUG: stop bit error.");
     end
   end
 end
