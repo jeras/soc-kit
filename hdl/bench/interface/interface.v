ff26e47a6274544a989c3815ea09aec8cbec41e3
jeras <iztok.jeras@gmail.com>
2009-05-14 00:07:11 +0200

    cleanup for vpi

--- hdl/bench/interface/interface.v
+++ hdl/bench/interface/interface.v
@@ -32,113 +32,33 @@ module interface #(
   parameter FNI = "",      // file name for input  signals (write file)
   parameter DT  = "hex",   // data type ("hex" (default), "bin", "raw")
   // presentation
-  parameter NAME = "noname",
-  parameter AUTO = 0
+  parameter NAME = "noname"
 )(
-  input  wire          c,  // clock
-  output reg  [NO-1:0] o,  // output signals
-  input  wire [NI-1:0] i   // input  signals
+  input  wire          clk,  // clock
+  input  wire          rst,  // clock
+  output reg  [NO-1:0] d_o,  // output signals
+  input  wire [NI-1:0] d_i   // input  signals
 );
-
-//////////////////////////////////////////////////////////////////////////////
-// local parameters and signals                                             //
-//////////////////////////////////////////////////////////////////////////////
-
-// master status
-reg     run = 0;   // master running status
-
-// file pointer and access status
-integer fpi, fsi = 0;  // write file
-integer fpo, fso = 0;  // read  file
-
-// program file parsing variables
-reg [3*8-1:0] cmd;
-
-reg [18*8-1:0] t;
-reg [7:0] ch;
-integer cnt;
-integer cnt_str;
 
 ///////////////////////////////////////////////////////////////////////////////
 // initialization                                                            //
 ///////////////////////////////////////////////////////////////////////////////
 
 initial begin
-  $display ("DEBUG: Starting master");
-  if (AUTO)  start (FNO, FNI);
+  $interface_init();
+  $interface_o;
+  $interface_i;
+  #7;
+  d_o = 8'h55;
+  #7;
+  d_o = d_i;
+// if (AUTO)  start (FNO, FNI);
 end
 
-task start (
-  input reg [256*8-1:0] fno,
-  input reg [256*8-1:0] fni
-); begin
-  // open file for output signals (read)  file
-  $display ("DEBUG: Opening output signals (read)  file: \"%0s\"", fno);
-  fpo = $fopen (fno, "r");
-  if (fpo)  $display ("DEBUG: File open SUCESS");
-  else      $display ("DEBUG: File open FAIL");
-  // open file for input  signals (write) file
-  $display ("DEBUG: Opening input  signals (write) file: \"%0s\"", fni);
-  fpi = $fopen (fni, "w");
-  if (fpi)  $display ("DEBUG: File open SUCESS");
-  else      $display ("DEBUG: File open FAIL");
+always @ (posedge rst, posedge clk)
+if (rst)  $interface_rst;
+else      $interface_clk;
 
-  for (cnt=0; cnt<8; cnt=cnt+1) begin
-    //interface_o;
-    //interface_i;
-    for (cnt_str=0; cnt_str<8; cnt_str=cnt_str+1) begin
-      ch = $fgetc (fpo);
-      $display("%s", ch);
-    end
-    $fwrite (fpi, "test message");
-    @ (posedge c);
-  end
-  $finish();
-//  interface_o;
-//  interface_i;
-
-//  run = 1;
-end endtask
-
-task stop; begin
-  run = 0;
-end endtask
-
-///////////////////////////////////////////////////////////////////////////////
-// implementation                                                            //
-///////////////////////////////////////////////////////////////////////////////
-
-//always @ (posedge c)
-//if (run) begin
-//  interface_o;
-//  interface_i;
-//end
-
-task interface_i; begin
-  //$fdisplay       (fpi, "%h", i);
-  //$fwrite         (fpi, "%h \n \n", i);
-  $fwrite         (fpi, "test message");
-  $display ("interface:i %h", i);
-end endtask
-
-task interface_o; begin
-  fso = 0;
-  while (fso == 0) begin
-    fso = $fscanf (fpo, "%h", o);
-    //$write ("%d", fso);
-    //$display ("DEBUG: Opening input  signals (write) file: \"%0d\"", fso);
-  end
-  $display ("interface:o %h", o);
-end endtask
 
 endmodule
-
-// read the first output values
-//  for (j=0; j<18; j=j+1) begin
-//    ch = $fgetc(fpo);
-//    $write ("\'%s\'", ch);
-//  end
-//  $display ("end of string");
-//  fso = $fread   (fpo, "%s", t);
-//  $display ("interface:o %s", t);
 
